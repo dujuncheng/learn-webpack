8e5e143b66584a82daa38a9666de417cb9508390
dujuncheng <1390851423@qq.com>
2018-02-07 01:07:05 +0800

    demo1

---
+++ demo/demo1/app.js
@@ -0,0 +1,12 @@
+
+// es6 module
+import sum from './sum'
+// commonjs 规范
+let min  = require('./minus')
+// AMD规范
+let muti =
+let
+let	x = 1
+let y = 2
+console.log(`x+y=${sum(x,y)}`)
+console.log(`x+y=${min(x,y)}`)
---
+++ demo/demo1/sum.js
@@ -0,0 +1,5 @@
+let sum = (x,y) => {
+	return x + y
+}
+
+export default sum
